428890960ee225f2d39fc47d42821dbecddf3f75
JDRV17 <123598113+JDRV17@users.noreply.github.com>
2025-10-16 12:16:53 -0500

    Update contract-158b311d57.sol

--- contract-158b311d57.sol
+++ contract-158b311d57.sol
@@ -7,12 +7,43 @@ import {ERC20Pausable} from "@openzeppelin/contracts@5.4.0/token/ERC20/extension
 import {ERC20Permit} from "@openzeppelin/contracts@5.4.0/token/ERC20/extensions/ERC20Permit.sol";
 import {Ownable} from "@openzeppelin/contracts@5.4.0/access/Ownable.sol";
 
-contract MyToken is ERC20, ERC20Pausable, Ownable, ERC20Permit {
-    constructor(address initialOwner)
-        ERC20("MyToken", "MTK")
+contract MagicToken is ERC20, ERC20Pausable, Ownable, ERC20Permit {
+    address public treasury;
+    uint8 public tax;
+    mapping(address => bool) public isFeeExempt;
+
+    event TaxFeeChanged(uint8 oldFee, uint8 newFee);
+    event TreasuryChanged(address oldTreasury, address newTreasury);
+    event FeeExemptionSet(address account, bool isExempt);
+
+    constructor(address initialOwner, address treasury_, uint8 taxFee_)
+        ERC20("MagicToken", "MAGIC")
         Ownable(initialOwner)
-        ERC20Permit("MyToken")
-    {}
+        ERC20Permit("MagicToken")
+    {
+        require(treasury_ != address(0), "Treasury cannot be zero address");
+        require(taxFee_ <= 100, "Fee cannot exceed 100%");
+        treasury = treasury_;
+        tax = taxFee_;
+        _mint(initialOwner, 1_000_000_000000000000000000);
+    }
+
+    function setTaxFee(uint8 newFee) external onlyOwner {
+        require(newFee <= 100, "Fee cannot exceed 100%");
+        emit TaxFeeChanged(tax, newFee);
+        tax = newFee;
+    }
+
+    function setTreasury(address newTreasury) external onlyOwner {
+        require(newTreasury != address(0), "Treasury cannot be zero address");
+        emit TreasuryChanged(treasury, newTreasury);
+        treasury = newTreasury;
+    }
+
+    function setFeeExempt(address account, bool exempt) external onlyOwner {
+        isFeeExempt[account] = exempt;
+        emit FeeExemptionSet(account, exempt);
+    }
 
     function pause() public onlyOwner {
         _pause();
@@ -22,12 +53,20 @@ contract MyToken is ERC20, ERC20Pausable, Ownable, ERC20Permit {
         _unpause();
     }
 
-    // The following functions are overrides required by Solidity.
-
-    function _update(address from, address to, uint256 value)
+    function _update(address from, address to, uint256 amount)
         internal
         override(ERC20, ERC20Pausable)
     {
-        super._update(from, to, value);
+        if (from != address(0) && to != address(0) && !paused()) {
+            if (!isFeeExempt[from] && !isFeeExempt[to] && tax > 0) {
+                uint256 fee = (amount * tax) / 100;
+                uint256 net = amount - fee;
+                super._update(from, treasury, fee);
+                super._update(from, to, net);
+                return;
+            }
+        }
+        super._update(from, to, amount);
     }
 }
+
